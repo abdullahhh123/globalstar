103b25907c654f685123c0a8ead852f2e4afa8d1
ibs <mohamedmady61@gmail.com>
2022-11-02 15:46:59 +0200

    add discount journal items

--- globalstar_customization/__manifest__.py
+++ globalstar_customization/__manifest__.py
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'stock', 'account'],
+    'depends': ['base', 'stock', 'account', 'sale'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/group.xml',
         'views/views.xml',
         # 'views/templates.xml',
         'demo/demo.xml',
